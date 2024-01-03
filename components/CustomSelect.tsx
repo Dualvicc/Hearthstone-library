58f1114097bab50709767c6a12fd5bd647690b5c
Dualvicc <vmpporci@gmail.com>
2024-01-03 14:37:15 +0100

    Feat/GetBearerToken WIP

--- components/CustomSelect.tsx
+++ components/CustomSelect.tsx
@@ -1,6 +1,22 @@
-export default function CustomSelect(){
-    return(
-        <>
-        </>
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/Select"
+
+export default function CustomSelect() {
+    return (
+        <Select>
+            <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Theme" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+        </Select>
     );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { getBearerToken } = require('@/app/auth.js');
+const fs = require('fs');
 
+require('dotenv').config();
+const nextConfig = {
+    env: {
+        CLIENT_ID: process.env.CLIENT_ID,
+        CLIENT_SECRET: process.env.CLIENT_SECRET,
+    },
+}
+exportPathMap: async function () {
+    try {
+        // Llama a la función para obtener el Bearer Token
+        const token = await getBearerToken();
+        console.log('Bearer Token:', token);
+
+        // Almacena el Bearer Token en el archivo .env
+        fs.appendFileSync('.env', `\nBEARER_TOKEN=${token}`);
+
+        // Resto de la lógica de exportación de rutas si es necesario
+        return {
+            // ...
+        };
+    } catch (error) {
+        console.error('Error al obtener el Bearer Token:', error);
+        throw error;
+    }
+}
 module.exports = nextConfig
